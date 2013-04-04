4b1bdfe938f66b0d5bbb5537efcc2ec6188d9344
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2013-04-04 23:35:23 +0200

    [iOS] do not specify iphonesdk version directly.

--- ios-client/build.sh
+++ ios-client/build.sh
@@ -10,6 +10,6 @@ echo `pwd`/$APP_NAME.ipa
 
 mkdir -p $BUILD_DIR
 export CONFIGURATION_BUILD_DIR=$BUILD_DIR
-xcodebuild -target $APP_NAME -sdk iphoneos6.0 -configuration Release
-/usr/bin/xcrun -sdk iphoneos6.0 PackageApplication -v build/Release-iphoneos/$APP_NAME.app -o "`pwd`/$APP_NAME.ipa"
-# --embed $PROVISION
+xcodebuild -target $APP_NAME -sdk iphoneos -configuration Release
+/usr/bin/xcrun -sdk iphoneos PackageApplication -v build/Release-iphoneos/$APP_NAME.app -o "`pwd`/$APP_NAME.ipa"
+
