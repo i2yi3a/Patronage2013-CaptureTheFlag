f4ae299bf12b4a1d59cd38c72238cea4be9cb78a
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2013-04-04 22:45:14 +0200

    Fixed build script

--- ios-client/build.sh
+++ ios-client/build.sh
@@ -11,5 +11,5 @@ echo `pwd`/$APP_NAME.ipa
 mkdir -p $BUILD_DIR
 export CONFIGURATION_BUILD_DIR=$BUILD_DIR
 xcodebuild -target $APP_NAME -sdk iphoneos6.0 -configuration Release
-/usr/bin/xcrun -sdk iphoneos6.0 PackageApplication -v build/Release-iphoneos/$APP_NAME.app -o `pwd`/$APP_NAME.ipa
+/usr/bin/xcrun -sdk iphoneos6.0 PackageApplication -v build/Release-iphoneos/$APP_NAME.app -o "`pwd`/$APP_NAME.ipa"
 # --embed $PROVISION
