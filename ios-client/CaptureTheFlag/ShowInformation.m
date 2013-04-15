da434f763f014355b4029c3d5c2e8d3a76cbd01d
Milena Gnoińska <milena.gnoinska@gmail.com>
2013-04-15 19:32:15 +0200

    [iOS] added ShowInformation class

---
+++ ios-client/CaptureTheFlag/ShowInformation.m
@@ -0,0 +1,13 @@
+//
+//  ShowInformation.m
+//  CaptureTheFlag
+//
+//  Created by Milena Gnoińska on 15.04.2013.
+//  Copyright (c) 2013 BLStream. All rights reserved.
+//
+
+#import "ShowInformation.h"
+
+@implementation ShowInformation
+
+@end
