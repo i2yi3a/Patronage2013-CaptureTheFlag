148e4cbe73167e14effd1cf445a2aabcde029da6
mateusz.karbowniczek <karboxylon@gmail.com>
2013-04-16 12:59:17 +0200

    Create Register page

---
+++ windows-phone-client/Ctf/Ctf/Register.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Ctf
+{
+    public partial class Register : PhoneApplicationPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+
+        }
+    }
+}
