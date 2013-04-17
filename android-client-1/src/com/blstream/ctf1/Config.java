c51447b00f11c63ebeda467b66a8d894d44abde0
Milosz Skalski <mskalski6@wp.pl>
2013-04-17 16:16:49 +0200

    added SplashScreen

---
+++ android-client-1/src/com/blstream/ctf1/Config.java
@@ -0,0 +1,5 @@
+package com.blstream.ctf1;
+
+public class Config {
+	public static final int SPLASHSCREEN_DELAY = 1000;
+}
---
+++ android-client-1/src/com/blstream/ctf1/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.blstream.ctf1;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        
+    	new Handler().postDelayed(new Thread() {
+    		@Override
+    		public void run() {
+    			Intent register = new Intent(SplashScreen.this, MainActivity.class);
+    			SplashScreen.this.startActivity(register);
+    			SplashScreen.this.finish();
+    		}
+    	}, Config.SPLASHSCREEN_DELAY);
+   
+    }
+    
+}
