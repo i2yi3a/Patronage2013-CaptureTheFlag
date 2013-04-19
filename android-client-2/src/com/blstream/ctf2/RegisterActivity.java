04bfbd4b6ec4e01ece90517065c4da3cf6f5df93
lukaszdmitrowski <lukasz.dmitrowski@gmail.com>
2013-04-19 17:58:28 +0200

    [MOD] Added methods for Google Analytics to RegisterActivity

--- android-client-2/src/com/blstream/ctf2/RegisterActivity.java
+++ android-client-2/src/com/blstream/ctf2/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.blstream.ctf2;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.EditText;
 
 /**
  * Player registration class
+ * 
  * @author Rafal Tatol
  */
 public class RegisterActivity extends Activity {
@@ -22,6 +25,18 @@ public class RegisterActivity extends Activity {
 		mUsernameEditText = (EditText) findViewById(R.id.editTextUserame);
 		mPasswordEditText = (EditText) findViewById(R.id.editTextPassword);
 		mRepeatPassEditText = (EditText) findViewById(R.id.editTextRepeatPassword);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EasyTracker.getInstance().activityStart(this);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this);
 	}
 
 	public void onClickRegisterButton(View v) {
