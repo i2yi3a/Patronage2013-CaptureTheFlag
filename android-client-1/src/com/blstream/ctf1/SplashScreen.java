f7ca19f6748c660390cc05331dbb5db47d1c103e
Milosz Skalski <mskalski6@wp.pl>
2013-04-22 11:58:06 +0200

    [FIX] Splashscreen

--- android-client-1/src/com/blstream/ctf1/SplashScreen.java
+++ android-client-1/src/com/blstream/ctf1/SplashScreen.java
@@ -24,7 +24,7 @@ public class SplashScreen extends Activity {
     			SplashScreen.this.startActivity(register);
     			SplashScreen.this.finish();
     		}
-    	}, Constans.SPLASHSCREEN_DELAY);
+    	}, Constants.SPLASHSCREEN_DELAY);
    
     }
     
