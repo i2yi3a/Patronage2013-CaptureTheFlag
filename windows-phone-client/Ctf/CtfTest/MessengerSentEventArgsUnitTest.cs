539c1831f3ecfdcbe93cb04c675c69e0ef0e3a48
Piotr Marczycki <piotrekm44@o2.pl>
2013-04-27 22:36:36 +0200

    [ADD][DEL] Added more unit test classes, deleted /CtfTest/Bin/ and /CtfTest/obj/

---
+++ windows-phone-client/Ctf/CtfTest/MessengerSentEventArgsUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Ctf;
+
+namespace CtfTest
+{
+    [TestClass]
+    public class MessengerSentEventArgsUnitTest
+    {
+        [TestMethod]
+        public void TestMessengerSentEventArgs()
+        {
+            Assert.Fail("TODO: Test MessengerSentEventArgs class.");
+        }
+    }
+}
