a1600e4daa1fcf4f761d9484ec89f95c36c9e5d4
Piotr Marczycki <piotrekm44@o2.pl>
2013-04-27 22:39:46 +0200

    [ADD] Added ErrorCode, LoginJsonResponse and ServerJsonResponse derived from JsonReponse

---
+++ windows-phone-client/Ctf/Ctf/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctf
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ErrorCode
+    {
+        public const int APPLICATION_ERROR = -1;
+        public const int SUCCESS = 0;
+    }
+}
---
+++ windows-phone-client/Ctf/Ctf/ServerJsonResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctf
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class ServerJsonResponse : JsonResponse
+    {
+        public int error_code { get; set; }
+    }
+}
