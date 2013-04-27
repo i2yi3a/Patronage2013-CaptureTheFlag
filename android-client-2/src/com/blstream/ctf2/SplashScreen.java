dd21c8bf4b5513da139aa2ef774dd6ad10533867
Marcin Szmit <mszmitek@gmail.com>
2013-04-27 19:34:59 +0200

    [FIX] SplashScreen Bug

--- android-client-2/src/com/blstream/ctf2/SplashScreen.java
+++ android-client-2/src/com/blstream/ctf2/SplashScreen.java
@@ -3,32 +3,36 @@ package com.blstream.ctf2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
- * SplashScreen class. 
- * Display splash screen for 2 seconds.
+ * SplashScreen class. Display splash screen for 2 seconds.
  * @author Marcin Szmit
  */
 public class SplashScreen extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+
+	private boolean mIsBackButtonPressed;
+
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
-		Thread splash = new Thread() {
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
 			public void run() {
-				try {
-					sleep(Constants.SPLASH_SHOW_TIME);
-					throw new InterruptedException();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
+				finish();
+				if (!mIsBackButtonPressed) {
 					Intent intent = new Intent(
 							"com.blstream.ctf2.LOGINACTIVITY");
 					startActivity(intent);
-					finish();
 				}
 			}
-		};
-		splash.start();
+		}, Constants.SPLASH_SHOW_TIME);
+	}
+
+	@Override
+	public void onBackPressed() {
+		mIsBackButtonPressed = true;
+		super.onBackPressed();
 	}
 }
