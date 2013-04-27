5dac3f852dc2f817d8428f966e5beeba714ceb7f
Piotr Marczycki <piotrekm44@o2.pl>
2013-04-27 19:34:52 +0200

    [ADD] Added test packages, project, ApplicationSettings and UserCredentials unit tests

---
+++ windows-phone-client/Ctf/CtfTest/ApplicationSettingsUnitTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Ctf;
+
+namespace CtfTest
+{
+    [TestClass]
+    public class ApplicationSettingsUnitTest
+    {
+        [TestMethod]
+        public void TestSaveLoggedUser()
+        {
+            string userKeyword = "user";
+            User nullUser = null;
+            User defaultUser = new User();
+            User properUser = new User("piotrekm44@o2.pl", "d53e57c6-cb45-481c-9ed4-df15803f0e0d", "bearer", "read write");
+            bool nullResult;
+            bool defaultResult;
+            bool firstProperResult;
+            bool secondProperResult;
+
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            nullResult = ApplicationSettings.Instance.SaveLoggedUser(nullUser);
+            defaultResult = ApplicationSettings.Instance.SaveLoggedUser(defaultUser);
+            firstProperResult = ApplicationSettings.Instance.SaveLoggedUser(properUser);
+            secondProperResult = ApplicationSettings.Instance.SaveLoggedUser(properUser);
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+
+            Assert.IsFalse(nullResult, "Null is saved.");
+            Assert.IsFalse(defaultResult, "Empty user is saved.");
+            Assert.IsTrue(firstProperResult, "User is not saved.");
+            Assert.IsFalse(secondProperResult, "User is overwritten.");
+        }
+
+        [TestMethod]
+        public void TestRetriveLoggedUser()
+        {
+            string userKeyword = "user";
+            User emptyResultUser = null;
+            User properResultUser = null;
+            User properUser = new User("piotrekm44@o2.pl", "d53e57c6-cb45-481c-9ed4-df15803f0e0d", "bearer", "read write");
+            bool properSavedResult;
+
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            emptyResultUser = ApplicationSettings.Instance.RetriveLoggedUser();
+            properSavedResult = ApplicationSettings.Instance.SaveLoggedUser(properUser);
+            properResultUser = ApplicationSettings.Instance.RetriveLoggedUser();
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+
+            Assert.IsTrue(emptyResultUser.HasNullOrEmpty(), "User should be empty.");
+            Assert.IsTrue(properSavedResult, "User is not saved.");
+            Assert.IsFalse(properResultUser.HasNullOrEmpty(), "User should NOT be empty.");
+        }
+
+        [TestMethod]
+        public void TestRemoveFromSettings()
+        {
+            string userKeyword = "user";
+            bool userNotRemoved;
+            User properUser = new User("piotrekm44@o2.pl", "d53e57c6-cb45-481c-9ed4-df15803f0e0d", "bearer", "read write");
+            bool properSavedResult;
+            bool userRemoved;
+
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            userNotRemoved = ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            properSavedResult = ApplicationSettings.Instance.SaveLoggedUser(properUser);
+            userRemoved = ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+
+            Assert.IsFalse(userNotRemoved, "No user inside but removal occured.");
+            Assert.IsTrue(properSavedResult, "User is not saved.");
+            Assert.IsTrue(userRemoved, "User was not removed.");
+        }
+
+        [TestMethod]
+        public void TestOnUserChanged()
+        {
+            string userKeyword = "user";
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            ApplicationSettings.Instance.UserChanged += HelperOnUserChanged;
+            User properUser = new User("piotrekm44@o2.pl", "d53e57c6-cb45-481c-9ed4-df15803f0e0d", "bearer", "read write");
+            ApplicationSettings.Instance.RemoveFromSettings(userKeyword);
+            ApplicationSettings.Instance.UserChanged -= HelperOnUserChanged;
+        }
+
+        public void HelperOnUserChanged(object sender, EventArgs e)
+        {
+            Assert.IsInstanceOfType(e, typeof(MessengerSentEventArgs), "EventArgs is not type of MessengerSentEventArgs");
+            string message = ((MessengerSentEventArgs)e).message;
+            Assert.IsNotNull(message, "Message should not be null.");
+        }
+    }
+}
---
+++ windows-phone-client/Ctf/CtfTest/UserCredentialsUnitTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Ctf;
+
+namespace CtfTest
+{
+    [TestClass]
+    public class UserCredentialsUnitTest
+    {
+        [TestMethod]
+        public void TestIsUsernameProperLength()
+        {
+            string properLengthInput = new string('a', UserCredentials.MINIMAL_USERNAME_LENGTH);
+            string improperLenghtInput = "a";
+            bool properLenghtResult;
+            bool improperLenghtResult;
+
+            properLenghtResult = UserCredentials.IsUsernameProperLength(properLengthInput);
+            improperLenghtResult = UserCredentials.IsUsernameProperLength(improperLenghtInput);
+
+            Assert.IsTrue(properLenghtResult, "Length test should pass.");
+            Assert.IsFalse(improperLenghtResult, "Length test should NOT pass.");
+        }
+
+        [TestMethod]
+        public void TestIsPasswordProperLength()
+        {
+            string properLengthInput = new string('a', UserCredentials.MINIMAL_PASSWORD_LENGTH);
+            string improperLenghtInput = "a";
+            bool properLenghtResult;
+            bool improperLenghtResult;
+
+            properLenghtResult = UserCredentials.IsPasswordProperLength(properLengthInput);
+            improperLenghtResult = UserCredentials.IsPasswordProperLength(improperLenghtInput);
+
+            Assert.IsTrue(properLenghtResult, "Length test should pass.");
+            Assert.IsFalse(improperLenghtResult, "Length test should NOT pass.");
+        }
+
+        [TestMethod]
+        public void TestHasMatchingPassword()
+        {
+            string properLengthInput = new string('a', UserCredentials.MINIMAL_PASSWORD_LENGTH);
+            string matchingPassword = new string('a', UserCredentials.MINIMAL_PASSWORD_LENGTH);
+            string notMatchingPassword = new string('b', UserCredentials.MINIMAL_PASSWORD_LENGTH);
+            UserCredentials userCredentials = new UserCredentials(properLengthInput, properLengthInput);
+            bool matchingPasswordResult;
+            bool notMatchingPasswordResult;
+
+            matchingPasswordResult = userCredentials.HasMatchingPassword(matchingPassword);
+            notMatchingPasswordResult = userCredentials.HasMatchingPassword(notMatchingPassword);
+
+            Assert.IsTrue(matchingPasswordResult, "Should be a match.");
+            Assert.IsFalse(notMatchingPasswordResult, "Should not be a match.");
+        }
+
+        [TestMethod]
+        public void TestUserCredentialsConstructor()
+        {
+            string properLengthInput = new string('a', UserCredentials.MINIMAL_PASSWORD_LENGTH);
+            string improperLenghtInput = "a";
+            int expectedMessageCount = 4;
+            UserCredentials.MessengerSent += HelperOnMessangerSent;
+            UserCredentials userCredentialsImpImp = new UserCredentials(improperLenghtInput, improperLenghtInput);
+            UserCredentials userCredentialsImpProp = new UserCredentials(improperLenghtInput, properLengthInput);
+            UserCredentials userCredentialsPropImp = new UserCredentials(properLengthInput, improperLenghtInput);
+            UserCredentials userCredentialsPropProp = new UserCredentials(properLengthInput, properLengthInput);
+            UserCredentials.MessengerSent -= HelperOnMessangerSent;
+
+            Assert.AreEqual(userCredentialsImpImp.GetUsername(), String.Empty);
+            Assert.AreEqual(userCredentialsImpImp.GetPassword(), String.Empty);
+            Assert.AreEqual(userCredentialsImpProp.GetUsername(), String.Empty);
+            Assert.AreEqual(userCredentialsImpProp.GetPassword(), properLengthInput);
+            Assert.AreEqual(userCredentialsPropImp.GetUsername(), properLengthInput);
+            Assert.AreEqual(userCredentialsPropImp.GetPassword(), String.Empty);
+            Assert.AreEqual(userCredentialsPropProp.GetUsername(), properLengthInput);
+            Assert.AreEqual(userCredentialsPropProp.GetPassword(), properLengthInput);
+            Assert.AreEqual(expectedMessageCount, messageCount, "Message count is not expected.");
+        }
+
+        private static int messageCount = 0;
+
+        public void HelperOnMessangerSent(object sender, EventArgs e)
+        {
+            Assert.IsInstanceOfType(e, typeof(MessengerSentEventArgs), "EventArgs is not type of MessengerSentEventArgs");
+            string message = ((MessengerSentEventArgs)e).message;
+            Assert.IsNotNull(message, "Message should not be null.");
+            messageCount++;
+        }
+    }
+}
