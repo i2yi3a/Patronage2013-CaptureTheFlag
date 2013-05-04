4695a1f06c33ec6eada8294a0d757c5a6e7c0196
Milosz Skalski <mskalski6@wp.pl>
2013-05-04 18:03:35 +0200

    [MOD] CreateGame - add function correctData

--- android-client-1/src/com/blstream/ctf1/CreateGameActivity.java
+++ android-client-1/src/com/blstream/ctf1/CreateGameActivity.java
@@ -1,14 +1,23 @@
 package com.blstream.ctf1;
+/**
+ * @author Milosz_Skalski
+ * @TODO in switch btnCreate: where to get: lat lng and radius
+ **/
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.blstream.ctf1.asynchronous.CreateGame;
+import com.blstream.ctf1.service.NetworkService;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class CreateGameActivity extends Activity implements OnClickListener {
@@ -47,34 +56,117 @@ public class CreateGameActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
 		switch (v.getId()) {
 		case R.id.btnCancel:
 			finish();
 			break;
 		case R.id.btnCreate:		
-			String gameName = mEditGameName.getText().toString();
-			String gameDescription = mEditGameDescription.getText().toString();
-			String locationName = mEditLocationName.getText().toString();
+			String mGameName = mEditGameName.getText().toString();
+			String mGameDescription = mEditGameDescription.getText().toString();
+			String mLocationName = mEditLocationName.getText().toString();
 			
-			//String startDate = mEditStartDate.getText().toString();
-			//String startTime = mEditStartTime.getText().toString();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-			SimpleDateFormat sdf2 = new SimpleDateFormat("m");
-			try {
-				Date startDate = sdf.parse(mEditStartDate.getText().toString());
-				Date startTime = sdf2.parse(mEditStartTime.getText().toString());
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
+			String mStartDate = mEditStartDate.getText().toString();
+			mStartDate = mStartDate.replace(".", "-");
+			mStartDate = mStartDate.replace("/", "-");
 			
-			int playingTime = Integer.parseInt(mEditPlayingTime.getText().toString());
-			int maxPlayers = Integer.parseInt(mEditMaxPlayers.getText().toString());
-			int maxPoints = Integer.parseInt(mEditMaxPoints.getText().toString());
+			String mStartTime = mEditStartTime.getText().toString();
+
+			String mPlayingTimeTmp = mEditPlayingTime.getText().toString();
+			String mMaxPlayersTmp = mEditMaxPlayers.getText().toString();
+			String mMaxPointsTmp = mEditMaxPoints.getText().toString();
 			
+			if(correctData(mGameName, mLocationName, mStartDate, mStartTime, mPlayingTimeTmp)){
+				if (NetworkService.isDeviceOnline(this)) {
+					Log.d("Data", "" +mStartDate + "  " + mStartTime);
+					
+					if(mMaxPlayersTmp.length() == 0)
+						mMaxPlayersTmp = "120"; // no limits?
+					if(mMaxPointsTmp.length() == 0)
+						mMaxPointsTmp = "120"; // no limits?
+					
+					long mPlayingTime = Integer.parseInt(mPlayingTimeTmp)*60*1000; // convert min on milliseconds
+					int mMaxPlayers = Integer.parseInt(mMaxPlayersTmp);
+					int mMaxPoints = Integer.parseInt(mMaxPointsTmp);
+					
+					CreateGame createGame = new CreateGame(this, CreateGameActivity.class, mGameName, mGameDescription, 
+							mStartDate + " " + mStartTime, mPlayingTime, mMaxPoints, mMaxPlayers, mLocationName, 0.0 ,0.0 ,1 ); // here change: lat lng and radius
+				}
+				else{
+					Toast.makeText(this, R.string.no_internet_connection, Toast.LENGTH_SHORT).show();
+				}
+			}
 			break;
 		}
+	}
+	
+	private boolean correctData(String gameName, String locationName, String mStartDate, String mStartTime, String playingTime){
+		String info = "";
+		boolean result = false;
+		
+		if(gameName.length() < 1){
+			info+=getResources().getString(R.string.game_name_too_short);
+		}
+		
+		if(locationName.length() < 1){
+			if(!info.isEmpty()) info += '\n';
+			info+=getResources().getString(R.string.location_name_too_short);
+		}
+				
+		if(mStartDate.length() < 1){
+			if(!info.isEmpty()) info += '\n';
+			info+=getResources().getString(R.string.start_date_too_short);			
+		} else {
+			int tmp = 0;
+			for(int i=0; i<mStartDate.length(); i++){
+				if(mStartDate.charAt(i) == '-') 
+					tmp++;
+			}
+			if(tmp != 2){
+				if(!info.isEmpty()) info += '\n';
+				info+=getResources().getString(R.string.start_date_incorrect);	
+			}else{
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy"); 
+				Date startDate = null;
+				try {
+					startDate = dateFormatter.parse(mStartDate);
+					Log.d("Data", startDate.toString());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					if(!info.isEmpty()) info += '\n';
+					info+=getResources().getString(R.string.start_date_incorrect);	
+				}	
+			}
+		}
+		
+		if(mStartTime.length() < 1){
+			if(!info.isEmpty()) info += '\n';
+			info+=getResources().getString(R.string.start_time_too_short);			
+		} else{ 
+			if( mStartTime.indexOf(":") == -1 || mStartTime.indexOf(":") == mStartTime.length()-1 || mStartTime.indexOf(":") == 0 || mStartTime.indexOf(":") != mStartTime.lastIndexOf(":") ){ //  no there ":" || ":" is end line || ":" is first char || ":" only one can be used
+				if(!info.isEmpty()) info += '\n';
+				info+=getResources().getString(R.string.start_time_incorrect);
+			}else{
+				Log.d("Data", "H:" + mStartTime.substring(0, mStartTime.indexOf(":")) + " M:" + mStartTime.substring(mStartTime.indexOf(":")+1, mStartTime.length()) );
+				if( Integer.parseInt( mStartTime.substring(0, mStartTime.indexOf(":")) ) > 23 || Integer.parseInt(mStartTime.substring(mStartTime.indexOf(":")+1, mStartTime.length()))  > 59 )  {
+					if(!info.isEmpty()) info += '\n';
+					info+=getResources().getString(R.string.start_time_number_too_large);					
+				}
+			}
+		}
+		
+		if(playingTime.length() < 1){
+			if(!info.isEmpty()) info += '\n';
+			info+=getResources().getString(R.string.playing_time_too_short);
+		}
+		
+		if (info.isEmpty()) {
+			result = true;
+		} else {
+			Toast.makeText(this, info, Toast.LENGTH_SHORT).show();
+		}
+		
+		return result;
 	}
 
 }
