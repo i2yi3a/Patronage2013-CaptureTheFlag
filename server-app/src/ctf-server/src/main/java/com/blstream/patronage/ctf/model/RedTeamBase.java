51090c75ca85dee75148f63edcb3f0c9b41c5582
Jackupl <jackkorniak@gmail.com>
2013-05-17 17:51:04 +0200

    CTFPAT-275 [Back-End] - Stworzenie obiektu reprezentującego bazy drużyn (czerwona, niebieska) FIX

--- server-app/src/ctf-server/src/main/java/com/blstream/patronage/ctf/model/RedTeamBase.java
+++ server-app/src/ctf-server/src/main/java/com/blstream/patronage/ctf/model/RedTeamBase.java
@@ -1,24 +1,6 @@
 package com.blstream.patronage.ctf.model;
 
 
-public class RedTeamBase {
+public class RedTeamBase extends BlueTeamBase{
 
-    private String name;
-    private Double[] latLng;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double[] getLatLng() {
-        return latLng;
-    }
-
-    public void setLatLng(Double[] latLng) {
-        this.latLng = latLng;
-    }
 }
