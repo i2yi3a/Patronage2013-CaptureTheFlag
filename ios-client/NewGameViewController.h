7578c52a0bb30fc417f2b72b7ea1cf2913ca7b64
Konrad Gnoinski <konrad.gnoinski@me.com>
2013-05-20 22:51:47 +0200

    CTFPAT-173 Przygotowanie ekranu pozwalajacego na edycje parametrow nowej gry

--- ios-client/NewGameViewController.h
+++ ios-client/NewGameViewController.h
@@ -7,8 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import<MapKit/MapKit.h>
 
-@interface NewGameViewController : UIViewController
+@interface NewGameViewController : UIViewController <MKMapViewDelegate,UIPickerViewDataSource,UIPickerViewDelegate> {
+    IBOutlet UIScrollView * Scroller;
+}
 - (IBAction)createNewGame:(id)sender;
+- (IBAction)geolocate:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UITextField *locationField;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLGeocoder *geocoder;
+@property (strong, nonatomic) NSArray *oneColumnList;
+@property (strong, nonatomic) NSArray *secondColumnList;
 
 @end
