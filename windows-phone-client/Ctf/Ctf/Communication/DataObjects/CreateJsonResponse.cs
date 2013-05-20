8cad543c64b885790a284fe3dd604b0cb442b2cd
Piotr Marczycki <piotrekm44@o2.pl>
2013-05-20 19:43:06 +0200

    Before merge

---
+++ windows-phone-client/Ctf/Ctf/Communication/DataObjects/CreateJsonResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctf.Communication.DataObjects
+{
+    class CreateJsonResponse: ServerJsonResponse
+    {
+        public string id { get; set; }
+    }
+}
