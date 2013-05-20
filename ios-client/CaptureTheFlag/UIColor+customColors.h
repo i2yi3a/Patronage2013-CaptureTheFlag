fc976b7496dd00a8aea44f657fb1b5861aa8e5b6
Sebastian Jędruszkiewicz <sebastian.jedruszkiewicz@blstream.com>
2013-05-20 18:49:57 +0200

    [ADD] Added custom colors category.

---
+++ ios-client/CaptureTheFlag/UIColor+customColors.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+customColors.h
+//  CaptureTheFlag
+//
+//  Created by Sebastian Jędruszkiewicz on 5/20/13.
+//  Copyright (c) 2013 BLStream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (customColors)
+
++ (UIColor*)ctfTextColor;
+
+@end
---
+++ ios-client/CaptureTheFlag/UIColor+customColors.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+customColors.m
+//  CaptureTheFlag
+//
+//  Created by Sebastian Jędruszkiewicz on 5/20/13.
+//  Copyright (c) 2013 BLStream. All rights reserved.
+//
+
+#import "UIColor+customColors.h"
+
+@implementation UIColor (customColors)
+
++ (UIColor*)ctfTextColor
+{
+    return [UIColor colorWithRed:220.0/255.0 green:220.0/255.0 blue:220.0/255.0 alpha:1.0];
+}
+
+@end
