4b9c48eb1b40821ef51f4def5a0724d9236161e7
Rafal Olichwer <rafal.olichwer@gmail.com>
2013-05-23 22:06:08 +0200

    [FIX] fixed bug with time

--- android-client-1/src/com/blstream/ctf1/Constants.java
+++ android-client-1/src/com/blstream/ctf1/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
 
 	public static final String DATE_FORMAT = "yyyy-MM-dd";
 
-	public static final String TIME_FORMAT = "hh:mm:ss";
+	public static final String TIME_FORMAT = "HH:mm:ss";
 	
 	public static final String EMPTY_STRING = "";
 	
