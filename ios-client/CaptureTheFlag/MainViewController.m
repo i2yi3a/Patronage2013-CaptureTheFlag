7fee6e584d775d998110444bb8270f2bff34e591
Konrad Gnoinski <konrad.gnoinski@me.com>
2013-05-29 10:26:20 +0200

    Jako uzytkownik chce sie wylogo... CTFPAT-197 UI do wylogowywania

--- ios-client/CaptureTheFlag/MainViewController.m
+++ ios-client/CaptureTheFlag/MainViewController.m
@@ -11,6 +11,8 @@
 #import "NewGameViewController.h"
 #import "GamesViewController.h"
 #import "MyGamesViewController.h"
+#import "NetworkEngine.h"
+
 
 @interface MainViewController ()
 
@@ -83,7 +85,9 @@
     }
     if (indexPath.row == 4)
     {
-        [self.navigationController popToRootViewControllerAnimated:YES];
+        [self logout:nil];
+        [self performSegueWithIdentifier:@"segueLogout" sender:self];
+
     }
 
 }
@@ -128,16 +132,7 @@
     return [self.mainViewControllerCellLabels count];
 }
 
-/*- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"MainViewCell";
@@ -172,45 +167,8 @@
     
     return 85.0f;
 }
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
+- (void)logout:(NetworkEngineCompletionBlock)completionBlock{
+}
 #pragma mark - Table view delegate
 
 /*- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
