b1e9163dbc49ce42dd1bbb69b077c34893ab7b28
Lukasz Dmitrowski <lukasz.dmitrowski@gmail.com>
2013-06-03 00:42:24 +0200

    [MOD]Added ProgressDialog instance

--- android-client-2/src/com/blstream/ctf2/services/DialogServices.java
+++ android-client-2/src/com/blstream/ctf2/services/DialogServices.java
@@ -1,21 +1,23 @@
 package com.blstream.ctf2.services;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 
 import com.blstream.ctf2.R;
 
 /**
- * 
  * @author Lukasz Dmitrowski
  */
 
 public class DialogServices {
 
 	private Context mCtx;
+	private ProgressDialog mConnectingDialog; 
 
 	public DialogServices(Context ctx) {
 		mCtx = ctx;
+		mConnectingDialog = new ProgressDialog(mCtx);
 	}
 
 	public void showAlert(int mAlertTextId) {
@@ -32,5 +34,20 @@ public class DialogServices {
 		builder.setPositiveButton(R.string.ok, null);
 		AlertDialog dialog = builder.create();
 		dialog.show();
+	}
+	
+	public void showConnectingDialog(){
+		mConnectingDialog.setIndeterminate(true);
+		mConnectingDialog.setCancelable(false);
+		mConnectingDialog.setMessage(mCtx.getResources().getString(R.string.connecting));
+		mConnectingDialog.show();
+	}
+	
+	public boolean checkConnectingDialog(){
+		return mConnectingDialog.isShowing();
+	}
+	
+	public void disableConnectingDialog(){
+		mConnectingDialog.dismiss();
 	}
 }
