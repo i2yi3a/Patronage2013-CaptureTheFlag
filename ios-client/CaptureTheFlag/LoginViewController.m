38128603ef3f5a2fbed0a010b5ec7ca785965ed3
Konrad Gnoinski <konrad.gnoinski@me.com>
2013-06-06 23:46:23 +0200

    ADD scrolling beetween textFields in loginViewController and registerViewController

--- ios-client/CaptureTheFlag/LoginViewController.m
+++ ios-client/CaptureTheFlag/LoginViewController.m
@@ -8,7 +8,10 @@
 
 
 #import "LoginViewController.h"
-
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
 
 @interface LoginViewController ()
 
@@ -22,6 +25,7 @@
 @property (weak, nonatomic) IBOutlet UIView *topBar;
 @property (weak, nonatomic) IBOutlet UIView *emailIconBackgroundView;
 @property (weak, nonatomic) IBOutlet UIView *passwordIconBackgroundView;
+@property (readwrite) CGFloat animatedDistance;
 
 - (IBAction)login:(id)sender;
 
@@ -73,7 +77,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 -(void)loginWithUserEmail:(NSString *)userEmail andPassword:(NSString *)password
 {
@@ -127,6 +130,55 @@
     [_userEmailField resignFirstResponder];
     [_passwordField resignFirstResponder];
 }
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    CGRect textFieldRect =
+    [self.view.window convertRect:textField.bounds fromView:textField];
+    CGRect viewRect =
+    [self.view.window convertRect:self.view.bounds fromView:self.view];
+    CGFloat midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
+    CGFloat numerator =
+    midline - viewRect.origin.y
+    - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    CGFloat denominator =
+    (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION)
+    * viewRect.size.height;
+    CGFloat heightFraction = numerator / denominator;if (heightFraction < 0.0)
+    {
+        heightFraction = 0.0;
+    }
+    else if (heightFraction > 1.0)
+    {
+        heightFraction = 1.0;
+    }
+    _animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y -= _animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += _animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
 
 - (void)viewDidUnload {
     [self setEmailBackgroundView:nil];
