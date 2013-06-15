bef6a613734729df6304e290d15f4b6a73e49cc6
Lukasz Dmitrowski <lukasz.dmitrowski@gmail.com>
2013-06-16 00:19:29 +0200

    [MOD] Functions separated

--- android-client-2/src/com/blstream/ctf2/services/DateTimeServices.java
+++ android-client-2/src/com/blstream/ctf2/services/DateTimeServices.java
@@ -2,6 +2,7 @@ package com.blstream.ctf2.services;
 
 import java.util.Calendar;
 
+import android.util.Log;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
@@ -10,56 +11,83 @@ import android.widget.TimePicker;
  */
 
 public class DateTimeServices {
-	public final static String dateTimeToString(TimePicker timeP, DatePicker dateP) {
+	public final static String dateToString(DatePicker dateP) {
 		Calendar c = Calendar.getInstance();
-		c.add(Calendar.DATE, -1);
-		timeP.setIs24HourView(true);
+		//c.add(Calendar.MONTH, -1);
 		int nowYear = c.get(Calendar.YEAR);
 		int nowMonth = c.get(Calendar.MONTH);
-		int nowDay = c.get(Calendar.DAY_OF_MONTH) + 1;
-		int nowHour = c.get(Calendar.HOUR_OF_DAY);
-		int nowMinute = c.get(Calendar.MINUTE);
+		int nowDay = c.get(Calendar.DAY_OF_MONTH);
 		int year = dateP.getYear();
-		int month = dateP.getMonth();
+		int month = dateP.getMonth() + 1;
+		String misiac = "" + month;
+		Log.i("Miesiac ustawiany: ",misiac );
 		int day = dateP.getDayOfMonth();
-		int hour = timeP.getCurrentHour();
-		int minute = timeP.getCurrentMinute();
-
-		String dateTime = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":00";
+		String date;
+		
+		if (month<10) date = year + "-" + "0" +  month + "-" + day + " ";
+		else  date = year + "-" + month + "-" + day + " ";
 
 		if (year > nowYear)
-			return dateTime;
+			return date;
 		if (year < nowYear)
 			return "";
 		if (month > nowMonth)
-			return dateTime;
+			return date;
 		if (month < nowMonth)
 			return "";
 		if (day > nowDay)
-			return dateTime;
+			return date;
 		if (day < nowDay)
 			return "";
-		if (hour > nowHour)
-			return dateTime;
-		if (hour < nowHour)
-			return "";
-		if (minute > nowMinute)
-			return dateTime;
 		else
 			return "";
 	}
-	
-	public final static void setDefaultTime(String dateTime, DatePicker dateP,TimePicker timeP){
-		int []timeArray = new int[5];
-		timeArray[0] = Integer.parseInt(dateTime.substring(0, dateTime.indexOf("-")));
-		timeArray[1] = Integer.parseInt(dateTime.substring(dateTime.indexOf("-") + 1, dateTime.lastIndexOf("-")));
-		timeArray[2] = Integer.parseInt(dateTime.substring(dateTime.lastIndexOf("-") + 1,dateTime.indexOf(" ")));
-		timeArray[3] = Integer.parseInt(dateTime.substring(dateTime.lastIndexOf(" ") + 1, dateTime.indexOf(":")));
-		timeArray[4] = Integer.parseInt(dateTime.substring(dateTime.indexOf(":") + 1, dateTime.lastIndexOf(":")));
-		
-		dateP.init(timeArray[0], timeArray[1]-1, timeArray[2], null);
+
+	public final static String timeToString(TimePicker timeP) {
+		Calendar c = Calendar.getInstance();
 		timeP.setIs24HourView(true);
-		timeP.setCurrentHour(timeArray[3]);
-		timeP.setCurrentMinute(timeArray[4]);
+		int nowHour = c.get(Calendar.HOUR_OF_DAY);
+		int nowMinute = c.get(Calendar.MINUTE);
+		int hour = timeP.getCurrentHour();
+		int minute = timeP.getCurrentMinute();
+		
+		String time = hour + ":" + minute + ":00";
+		
+		
+		if (hour > nowHour)
+			return time;
+		if (hour < nowHour)
+			return "";
+		if (minute > nowMinute)
+			return time;
+		else
+			return "";
+	}
+
+	public final static void setDefaultDate(String date, DatePicker dateP) {
+		Calendar c = Calendar.getInstance();
+		int day = 0;
+		int month = 0;
+		int year = 0;
+		year = Integer.parseInt(date.substring(0,4));
+		month = Integer.parseInt(date.substring(5,7));
+		day = Integer.parseInt(date.substring(8,10));
+		
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, month);
+		c.set(Calendar.DAY_OF_MONTH, day);
+		c.add(Calendar.MONTH, -1);
+		dateP.init(c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH), null);
+	}
+
+	public final static void setDefaultTime(String time, TimePicker timeP) {
+		int hour = 0;
+		int minute = 0;
+		hour = Integer.parseInt(time.substring(0,2));
+		minute = Integer.parseInt(time.substring(3,5));
+
+		timeP.setIs24HourView(true);
+		timeP.setCurrentHour(hour);
+		timeP.setCurrentMinute(minute);
 	}
 }
